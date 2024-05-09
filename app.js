58054aa6274be74c651e011a15cc397ecebf1fd3
Christopher Schmidt <christopher.1998@gmx.net>
2024-05-09 19:23:57 +0200

    jo

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
 let time;
 let interval;
+let audio = new Audio("alarm.mp3")
 
 function startTimer() {
     const minutes = document.getElementById('timeInput').value;
     time = minutes * 60;
     updateTimerDisplay(time);
     interval = setInterval(decrementTimer, 1000);
+    document.getElementById('pauseButton').style.display = 'inline';
+    document.getElementById('continueButton').style.display = 'none';
+    document.getElementById('stopButton').style.display = 'inline';
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('timerDisplay').style.display = 'inline';
+    
+    
 }
 
 function pauseTimer() {
     clearInterval(interval);
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('continueButton').style.display = 'inline';
+    document.getElementById('stopButton').style.display = 'inline';
+    document.getElementById('startButton').style.display = 'none';
+}
+
+function continueTimer() {
+    interval = setInterval(decrementTimer, 1000);
+    document.getElementById('pauseButton').style.display = 'inline';
+    document.getElementById('continueButton').style.display = 'none';
+    document.getElementById('stopButton').style.display = 'inline';
+    document.getElementById('startButton').style.display = 'none';
 }
 
 function stopTimer() {
     clearInterval(interval);
     time = 0;
-    updateTimerDisplay(time);
-    alert("Timer gestoppt!");
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('continueButton').style.display = 'none';
+    document.getElementById('stopButton').style.display = 'none';
+    document.getElementById('startButton').style.display = 'inline';
+    document.getElementById('timerDisplay').style.display = 'none';
+}
+
+function playMusic() {
+    audio.play()
+}
+
+function stopMusic() {
+    audio.pause();
+    audio.currentTime = 0;
 }
 
 function decrementTimer() {
     time--;
     updateTimerDisplay(time);
-    if (time < 0) {
+    if (time <= 0) {
         clearInterval(interval);
-        alert("Zeit abgelaufen!");
+        document.getElementById('stopPlay').style.display = 'inline';
+        playMusic();
     }
 }
 
@@ -35,12 +68,4 @@ function updateTimerDisplay(seconds) {
     document.getElementById('timerDisplay').textContent = `${minutes}:${remainderSeconds}`;
 }
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+
